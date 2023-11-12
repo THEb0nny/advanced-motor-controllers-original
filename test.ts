4e5ddad57b2d4ae599a7e494db622c9562b6759d
Dmitriy Antipov <aston_bonny@mail.ru>
2023-11-12 16:45:17 +0300

    test

--- test.ts
+++ test.ts
@@ -1,1 +1,27 @@
-// здесь помещаются тесты; они не будут скомпилированы, пока пакет используется в качестве расширения.
+function Main() {
+    motors.mediumB.setInverted(true);
+    motors.mediumB.setBrake(true); motors.mediumC.setBrake(true);
+    //motors.mediumB.setRegulated(false); motors.mediumC.setRegulated(false);
+    motors.mediumB.clearCounts(); motors.mediumC.clearCounts();
+    brick.buttonEnter.pauseUntil(ButtonEvent.Pressed);
+    advmotctrls.AccTwoEnc_Config(10, 90, 300, 300, 1000);
+    advmotctrls.Sync_Config(0.25, 25, 50);
+    while (true) {
+        let encB = motors.mediumB.angle();
+        let encC = motors.mediumC.angle();
+        /*
+        let motorsPow = advmotctrls.Sync(encB, encC);
+        motors.mediumB.run(motorsPow[0]);
+        motors.mediumC.run(motorsPow[1]);
+        if ((encB + encC) / 2 >= 600) break;
+        */
+        let out = advmotctrls.AccTwoEnc(encB, encC);
+        motors.mediumB.run(out[0])
+        motors.mediumC.run(out[0]);
+        if (out[1]) break;
+    }
+    motors.mediumB.stop();
+    motors.mediumC.stop();
+}
+
+Main();
